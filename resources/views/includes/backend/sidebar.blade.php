6b9f7992e699c9d7b5983adbb80a3509727d9b09
wikukarno <prasetyagama2@gmail.com>
2023-07-23 16:35:38 +0700

    update

--- resources/views/includes/backend/sidebar.blade.php
+++ resources/views/includes/backend/sidebar.blade.php
@@ -7,8 +7,8 @@
           <span class="login-status online"></span>
         </div>
         <div class="nav-profile-text d-flex flex-column">
-          <span class="font-weight-bold mb-2">{{ Auth::user()->name }}</span>
-          <span class="text-secondary text-small">{{ Auth::user()->role }}</span>
+          <span class="font-weight-bold mb-2">{{ Auth::user()->name ?? '' }}</span>
+          <span class="text-secondary text-small">{{ Auth::user()->role ?? '' }}</span>
         </div>
         <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
       </a>
