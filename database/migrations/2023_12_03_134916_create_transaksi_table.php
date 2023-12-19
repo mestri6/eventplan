e5a9310020f68258cc28108c815ed53184527319
wikukarno <prasetyagama2@gmail.com>
2023-12-19 23:31:46 +0700

    update db

--- database/migrations/2023_12_03_134916_create_transaksi_table.php
+++ database/migrations/2023_12_03_134916_create_transaksi_table.php
@@ -13,15 +13,16 @@ return new class extends Migration
     {
         Schema::create('transaksi', function (Blueprint $table) {
             $table->id('id_transaksi');
-            $table->string('id_layanan');
-            $table->string('id_user');
-            $table->string('id_order');
+            $table->string('id_layanan')->nullable();
+            $table->string('id_user')->nullable();
+            $table->string('id_order')->nullable();
             $table->date('tanggal_acara');
             $table->string('alamat');
             $table->enum('status_pembayaran', ['tertunda', 'berhasil', 'gagal'])->default('tertunda');
             $table->string('bukti_pembayaran')->nullable();
             $table->string('kode_unik')->nullable();
             $table->integer('total_pembayaran')->nullable();
+            $table->string('midtrans_url')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
